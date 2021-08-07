833949f41af27547b96a91bc24b7ddf1bec01e42
Bruno Zablotny <rusholdfield@gmail.com>
2021-08-07 07:49:16 +0200

    Fighting with ghpages deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { RouterModule, Routes } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
+import { AppComponent } from './app.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
@@ -26,6 +13,27 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { UserProfileUpdateComponent } from './user-profile-update/user-profile-update.component';
 import { UserProfileDeleteComponent } from './user-profile-delete/user-profile-delete.component';
+
+// Routing and Http
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+// Materials for styling
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
@@ -49,21 +57,24 @@ const appRoutes: Routes = [
     UserProfileDeleteComponent,
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
-    BrowserModule,
-    MDBBootstrapModule,
     AppRoutingModule,
-    HttpClientModule,
+    BrowserModule,    
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     MatInputModule,
-    MatIconModule,
     MatButtonModule,
     MatCardModule,
-    FlexLayoutModule,
     MatFormFieldModule,
-    MatSnackBarModule,
     MatDialogModule,
-    FormsModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    RouterModule.forRoot(appRoutes),
+    MDBBootstrapModule,
+    FlexLayoutModule
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent],
--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -14,31 +14,16 @@ const apiUrl = 'https://brunoza-api.herokuapp.com/';
 
 //User Registration
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class FetchApiDataService {
-  /**
-   *
-   * @param http
-   * @param router
-   */
   constructor(private http: HttpClient, private router: Router) {}
-  /**
-   * API call to register new user account
-   * @param userDetails
-   * @returns
-   */
+
   userRegistration(userDetails: any): Observable<any> {
     return this.http
       .post(apiUrl + 'users', userDetails)
       .pipe(catchError(this.handleError));
   }
-  /**
-   * Handles user login HTTP request
-   * @param userDetails
-   * @returns
-   */
 
   //User Login
   userLogin(userDetails: any): Observable<any> {
@@ -55,10 +40,6 @@ export class FetchApiDataService {
       .pipe(catchError(this.handleError));
   }
 
-  /**
-   * API call to fetch all movies in database
-   * @returns
-   */
   //Get All Movies
   getAllMovies(): Observable<any> {
     const token = localStorage.getItem('token');
@@ -164,6 +145,7 @@ export class FetchApiDataService {
     const body = res;
     return body || {};
   }
+
   private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
       console.error('Some error occurred:', error.error.message);
